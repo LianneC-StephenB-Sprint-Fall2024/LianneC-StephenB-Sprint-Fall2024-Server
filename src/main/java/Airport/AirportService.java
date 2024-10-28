e4b3b4f7f63167aa59d24bddd24df4a87faed364
LianneCampbell <lianne.campbell@keyin.com>
2024-10-28 21:26:03 -0230

    Extend airport with controller repository and service and update pom file

---
+++ src/main/java/Airport/AirportService.java
@@ -0,0 +1,15 @@
+package Airport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AirportService {
+
+    @Autowired
+    private AirportRepository airportRepository;
+
+    public Iterable<Airport> getAllAirports() {
+        return airportRepository.findAll();
+    }
+}
