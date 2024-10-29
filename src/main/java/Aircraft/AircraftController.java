8488b2f8c0684202e0be17051f9685f40cbcbc71
LianneCampbell <lianne.campbell@keyin.com>
2024-10-29 12:56:23 -0230

    Extend Aircraft entity with controller,repo,service

---
+++ src/main/java/Aircraft/AircraftController.java
@@ -0,0 +1,40 @@
+package Aircraft;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/aircraft")
+@CrossOrigin
+public class AircraftController {
+
+    @Autowired
+    private AircraftService aircraftService;
+
+    @GetMapping
+    public List<Aircraft> getAllAircraft() {
+        return aircraftService.getAllAircraft();
+    }
+
+    @GetMapping("/{id}")
+    public Aircraft getAircraftById(@PathVariable Integer id) {
+        return aircraftService.getAircraftById(id);
+    }
+
+    @PostMapping
+    public Aircraft createAircraft(@RequestBody Aircraft aircraft) {
+        return aircraftService.createAircraft(aircraft);
+    }
+
+    @PutMapping("/{id}")
+    public Aircraft updateAircraft(@PathVariable Integer id, @RequestBody Aircraft aircraft) {
+        return aircraftService.updateAircraft(id, aircraft);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAircraft(@PathVariable Integer id) {
+        aircraftService.deleteAircraft(id);
+    }
+}
---
+++ src/main/java/Aircraft/AircraftService.java
@@ -0,0 +1,44 @@
+package Aircraft;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AircraftService {
+
+    @Autowired
+    private AircraftRepository aircraftRepository;
+
+    public List<Aircraft> getAllAircraft() {
+        return aircraftRepository.findAll();
+    }
+
+    public Aircraft getAircraftById(Integer id) {
+        return aircraftRepository.findById(id).orElse(null);
+    }
+
+    public Aircraft createAircraft(Aircraft aircraft) {
+        return aircraftRepository.save(aircraft);
+    }
+
+    public Aircraft updateAircraft(Integer id, Aircraft aircraftDetails) {
+        Optional<Aircraft> aircraftOptional = aircraftRepository.findById(id);
+
+        if (aircraftOptional.isPresent()) {
+            Aircraft existingAircraft = aircraftOptional.get();
+            existingAircraft.setType(aircraftDetails.getType());
+            existingAircraft.setAirlineName(aircraftDetails.getAirlineName());
+            existingAircraft.setNumberOfPassengers(aircraftDetails.getNumberOfPassengers());
+            return aircraftRepository.save(existingAircraft);
+        } else {
+            return null; // Could handle a 'not found' situation more gracefully
+        }
+    }
+
+    public void deleteAircraft(Integer id) {
+        aircraftRepository.deleteById(id);
+    }
+}
