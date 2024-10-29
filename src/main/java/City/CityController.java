2df9114786c4a9d7da6228f36b202a016a5f3a9b
LianneCampbell <lianne.campbell@keyin.com>
2024-10-29 13:02:20 -0230

    Extend City entity with controller,repo,service

---
+++ src/main/java/City/CityController.java
@@ -0,0 +1,40 @@
+package City;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cities")
+@CrossOrigin
+public class CityController {
+
+    @Autowired
+    private CityService cityService;
+
+    @GetMapping
+    public List<City> getAllCities() {
+        return cityService.getAllCities();
+    }
+
+    @GetMapping("/{id}")
+    public City getCityById(@PathVariable Integer id) {
+        return cityService.getCityById(id);
+    }
+
+    @PostMapping
+    public City createCity(@RequestBody City city) {
+        return cityService.createCity(city);
+    }
+
+    @PutMapping("/{id}")
+    public City updateCity(@PathVariable Integer id, @RequestBody City city) {
+        return cityService.updateCity(id, city);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCity(@PathVariable Integer id) {
+        cityService.deleteCity(id);
+    }
+}
