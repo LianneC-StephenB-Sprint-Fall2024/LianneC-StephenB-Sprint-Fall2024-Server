f93a6ac13d5416446337ec3aec762249c6a3bf66
LianneCampbell <lianne.campbell@keyin.com>
2024-11-01 09:42:05 -0230

    Changed import to jakarta - resolved

--- src/main/java/com/keyin/airport/Airport.java
+++ src/main/java/com/keyin/airport/Airport.java
@@ -1,6 +1,9 @@
 package com.keyin.airport;
 
-import javax.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Airport {
--- src/main/java/com/keyin/city/City.java
+++ src/main/java/com/keyin/city/City.java
@@ -1,6 +1,9 @@
 package com.keyin.city;
 
-import javax.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class City {
